350665ce2190e25ee16d59914b1c6cee66a5d26d
Guilherme Andrade <andrade@rarolabs.com.br>
2019-06-11 13:57:15 -0300

    Multer para imagens enviadas

---
+++ src/controllers/LikeController.js
@@ -0,0 +1,9 @@
+const Post = require('../models/Post');
+module.exports = {
+  async store(req, res) {
+    const post = await Post.findById(req.params.id);
+    post.likes += 1;
+    await post.save();
+    res.json(post);
+  }
+}
